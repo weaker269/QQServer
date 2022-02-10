f764860ab6bf6fc8c49cfb272e1facca67fff61e
weaker269 <1366905044@qq.com>
2022-02-10 23:56:23 +0800

    get and set

--- src/com/wct/QQCommon/User.java
+++ src/com/wct/QQCommon/User.java
@@ -5,4 +5,27 @@ package com.wct.QQCommon;
  * 表示一个用户信息
  */
 public class User {
+    private String uid;//用户id
+    private String password;//用户密码
+
+    public User(String uid, String password) {
+        this.uid = uid;
+        this.password = password;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
